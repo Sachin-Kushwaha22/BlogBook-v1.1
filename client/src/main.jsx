c05a814821410da5c902440cf4ffee05497e1cb4
Sachin Kushwaha <sachin2219n@gmail.com>
2025-02-23 22:10:06 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -39,7 +39,7 @@ createRoot(document.getElementById('root')).render(
           theme="dark"
           transition={Bounce} />
         <Routes>
-          <Route path="/admin" element={<Admin />} />
+{/*           <Route path="/admin" element={<Admin />} /> */}
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
           {/* <Route path="/signin" element={<Signin />} />
@@ -47,7 +47,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/readBlog/:id" element={<Readblog />} />
           <Route path="/writeBlog" element={<Writeblog />} />
           <Route path="/user/profile" element={<Profile />} />
-          <Route path="/user/dashboard/test2" element={<Dashboard />} />
+{/*           <Route path="/user/dashboard/test2" element={<Dashboard />} /> */}
           {/* <Route path="/user/dashboard/test" element={<UserDashboard />} /> */}
           <Route path="/user/dashboard" element={<UserDashboard />} />
         </Routes>
